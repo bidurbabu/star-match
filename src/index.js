be9f16c2269e583a443a800cb5c2aa67e94dcaa0
Bidur Adhikari <badhikari@godaddy.com>
2019-03-17 21:33:20 -0700

    Basic element optimization 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import css from './style.css'
 
 
 const StarMatch = () => {
+  const [stars, setState] = useState(utils.random(1,9));
+  const numbers = 9;
     return (
       <div className="game">
         <div className="help">
@@ -11,26 +13,13 @@ const StarMatch = () => {
         </div>
         <div className="body">
           <div className="left">
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
+          { utils.range(1,stars)
+          .map(
+            (starId)=> { return (<div key={starId} className="star" />)
+           }) }
           </div>
           <div className="right">
-            <button className="number">1</button>
-            <button className="number">2</button>
-            <button className="number">3</button>
-            <button className="number">4</button>
-            <button className="number">5</button>
-            <button className="number">6</button>
-            <button className="number">7</button>
-            <button className="number">8</button>
-            <button className="number">9</button>
+           {utils.range(1,numbers).map(number => <button key={number} className="number">{number}</button>)}
           </div>
         </div>
         <div className="timer">Time Remaining: 10</div>
